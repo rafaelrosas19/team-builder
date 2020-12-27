87db3137cebb584b755ba46116741c75d10e17d2
El Rafa <rafaelrosas19@gmail.com>
2020-12-26 20:44:46 -0600

    Added Manager subclass

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(firstname, lastname, id, email, role, officenumber ){
+        super(firstname, lastname, id, email, role);
+        this.officeNumber = officenumber;
+    }
+
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+};
+
+module.exports = Manager;
