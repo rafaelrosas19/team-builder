3b940fd558be419e527d50031950aba0c3dc8891
El Rafa <rafaelrosas19@gmail.com>
2020-12-28 21:59:00 -0600

    Added a default parameter for role as Employee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,6 +1,6 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
-    constructor(name, id, email, role) {
+    constructor(name, id, email, role = "Employee") {
         this.name = name;
         this.id = id;
         this.email = email;
